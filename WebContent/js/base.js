4879c1ce2bac29847a4b62f5a0e6537cebfca951
jbmackin <jbmackin@loyola.edu>
2016-07-28 14:51:47 -0400

    Let there be connections
    Connections have been made. I can move from the javascript to the java
    code to the database itself. Then when I need to pull things back down
    from the database, I have those capabilities too. Now to do events and
    further UI development. And I also need to figure out exactly what
    fields need to belong to each because I don’t think that I have exactly
    what is desired.

--- WebContent/js/base.js
+++ WebContent/js/base.js
@@ -1,10 +1,32 @@
-
+/**	
+ * 	Base functions that are applicable for more than one page
+ */
 
 function loadHome()
 {
 	console.log($(document));
+	$.ajax({
+		type: 'POST',
+		url: 'Home', 
+		data: 
+		{
+			'action': 'getHomeData',
+		},
+		success: function(data)
+		{
+			console.log(data);
+		},
+		error: function(xhr)
+		{
+			console.log(xhr.responseText);
+		}
+	});
 }
 
+/**
+ * The title bar calls navigate in order to navigate to the base page of each one
+ * @param location - the name of the destination page
+ */
 function navigate(location)
 {
 	switch(location.getAttribute('id'))
@@ -26,15 +48,39 @@ function navigate(location)
 	}
 }
 
+/**
+ * The mentorship's submenu uses this to navigate with filters already in place
+ * @param param - the major that the page will filter for 
+ */
 function findMentorships(param)
 {
 	console.log("navigating with parameter " + param);
 	window.location.href = "mentorships.html";
 }
 
+/**
+ * The internship's submenu uses this to navigate with filters already in place
+ * @param param - the major that the page will filter for
+ */
 function findInternships(param)
 {
 	console.log("navigating with parameter " + param);
 	window.location.href = "internships.html";
 }
+
+/**
+ * 	Expands tabs on the mentorship, internships, and events pages
+ * 	TODO: use html classes to generalize this ie. find all children with class (.hideme) and toggle hidden 
+ */
+function expand(tab)
+{
+	$(tab).siblings().children(".name").addClass("hidden");
+	$(tab).siblings().removeClass('active');
+		
+	$(tab).children().removeClass("hidden");
+	$(tab).addClass('active');
+}
+
+
+
 
