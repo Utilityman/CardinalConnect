098cb6a0f49b44e50a44284b7e38760bbaf60020
jbmackin <jbmackin@loyola.edu>
2017-03-21 17:00:26 -0400

    Working on index
    Mostly just templating for future work to base itself on.
    
    Once we get this page, the rest will be easy.

--- config.js
+++ config.js
@@ -2,9 +2,24 @@
 (function () {
   'use strict';
 
-  let Config = function () {
+  let Globals = function () {
     this.MONGO_URL = 'mongodb://localhost:27017/cardinal-connect';
   };
 
-  module.exports = Config;
+  Globals.prototype.createUserObject = function (json, callback) {
+
+    // TODO: data validation
+    let user = {
+      'active': 0,
+      'company': '',
+      'email': json.email,
+      'password': json.password,
+      'firstName': json.firstName,
+      'lastName': json.lastName,
+      'role': json.status.toLowerCase()
+    }
+    return user;
+  }
+
+  module.exports = Globals;
 })();
--- routes/index.js
+++ routes/index.js
@@ -3,72 +3,117 @@
 let express = require('express');
 let router = express.Router();
 let mongodb = require('mongodb');
-let Config = require('../config');
-let globals = new Config();
+let Globals = require('../config');
+let globals = new Globals();
 
 router.post('/Login', function (req, res, next) {
-  let json = req.body;
-
-  let action = json.action;
-  let username = json.username;
-  let password = json.password;
-
-  //res.send('hello');
-  login(action, username, password, res);
+  let t0 = new Date().getTime();
+  login(req.body, function(response) {
+    let t1 = new Date().getTime();
+    console.log('POST@/Login --- Response:' + response + ' --- ' + (t1 - t0) + 'ms');
+    res.send(response);
+  });
 });
 
-function login(reqAction, username, password, res) {
-  if(reqAction !== 'login') {
-    return 'INCORRECT_ACTION_TYPE';
+router.post('/Register', function (req, res, next) {
+  console.log(req.body);
+  let t0 = new Date().getTime();
+  register(req.body, function (response) {
+    let t1 = new Date().getTime();
+    console.log('POST@/Register --- Response:' + response + ' --- ' + (t1 - t0) + 'ms');
+    res.send(response);
+  });
+});
+
+function login(json, callback) {
+  if(json.action !== 'login') {
+    callback('INCORRECT_ACTION_TYPE');
   } else {
+    let username = json.username;
+    let password = json.password;
     let MongoClient = mongodb.MongoClient;
 
     MongoClient.connect(globals.MONGO_URL, function (err, db) {
       if (err) {
         console.log(err);
+        callback('SERVER_FAILURE');
       } else {
         let collection = db.collection('users');
 
-        collection.find({}).toArray(function (err, result) {
+        collection.find({email: username}).toArray(function (err, result) {
+          // if error, server failure
           if (err) {
             db.close();
-            res.send('DB_ERROR');
-          } else if (result.length) {
+            callback('SERVER_FAILURE');
 
-            for(let i = 0; i < result.length; i++) {
-              if(result[i].email === username &&
-                 result[i].password === password) {
-              db.close();
-              res.send('LOGIN_SUCCESS');
+          } else if (result.length) { // else if there are users in the database
+            // if there is one result
+            if (result.length === 1 && result[0].email === username) {
+              if (result[0].active === 0) {
+                // if the account is inactive, end login process
+                callback('ACCOUNT_INACTIVE');
+              } else if (result[0].password === password) {
+                // if query user password is the password, login
+                if (result[0].role === 'admin') {
+                  // TODO: Set session as valid
+                  callback('LOGIN_ADMIN');
+                } else {
+                  callback('LOGIN_SUCCESS');
+                }
+              } else {
+                // else, invalid login
+                callback('INVALID_LOGIN');
               }
+            } else {
+              console.log('err: found multiple users with the same email!');
+              callback('SERVER_FAILURE');
             }
             db.close();
-            res.send('INVALID_LOGIN');
-          } else {
+          } else {  // if the user is not the database (invalid id)
+            callback('INVALID_LOGIN');
             db.close();
-            res.send( 'NO_USERS');
           }
         });
       }
     });
-  }
-}
+  } // end else
+} // end login
 
-/*function register(json) {
-  MongClient.connect(ur, function(err, db) {
-    let collection = db.collection('users');
-
-    let user = {email: json.email, firstName: json.firstName};
-
-    collection.insert([user], function (err, result) {
+function register(json, callback) {
+  if(json.action !== 'register') {
+    console.log(json.action);
+    callback('INCORRECT_ACTION_TYPE');
+  } else {
+    let MongoClient = mongodb.MongoClient;
+    MongoClient.connect(globals.MONGO_URL, function (err, db) {
       if (err) {
         console.log(err);
+        callback('SERVER_FAILURE');
       } else {
-        db.close();
-        res.send('you done good kid');
+        let collection = db.collection('users');
+
+        let user = globals.createUserObject(json, function(err) {
+          if(err) {
+            console.log('woa');
+          }
+        });
+        //console.log(user);
+        callback('TODO_SAVE_USER');
       }
+      /*let collection = db.collection('users');
+
+      let user = {email: json.email, firstName: json.firstName};
+
+      collection.insert([user], function (err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          db.close();
+          res.send('you done good kid');
+        }
+      });*/
     });
-  })
-}*/
+  }
+}
 
 module.exports = router;
