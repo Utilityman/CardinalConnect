7bec79c208c6ccc3d04320d8cd7e8491fb4b674d
Christian Hew <cahew@loyola.edu>
2017-04-03 18:04:22 -0500

    admin star

--- routes/admin.js
+++ routes/admin.js
@@ -17,4 +17,55 @@ function login(reqAction, username, password) {
   }
 }
 
+router.post('/AcceptIntership', function (req, res, next) {
+	  let t0 = new Date().getTime();
+	  acceptInternship(req.body, function(response) {
+	    let t1 = new Date().getTime();
+	    console.log('POST@/AcceptIntership --- Response:' + response + ' --- ' + (t1 - t0) + 'ms');
+	    res.send(response);
+	  });
+	});
+
+router.post('/AcceptMentoship', function (req, res, next) {
+	  let t0 = new Date().getTime();
+	  acceptMentorship(req.body, function(response) {
+	    let t1 = new Date().getTime();
+	    console.log('POST@/AcceptMentorship --- Response:' + response + ' --- ' + (t1 - t0) + 'ms');
+	    res.send(response);
+	  });
+	});
+
+
+
+function AcceptInternship(json, callback) {
+	  if(json.action !== 'acceptInternship') {
+	    callback('INCORRECT_ACTION_TYPE');
+	  } else {
+	    let MongoClient = mongodb.MongoClient;
+	    MongoClient.connect(globals.MONGO_URL, function (err, db) {
+	      if (err) {
+	        console.log(err);
+	        callback('SERVER_ERROR');
+	      } else {
+	        globals.createInternshipObject(json, function(err, internship) {
+	          if(err) {
+	            console.log('err@internships.js:acceptInternship() - ' + err);
+	            callback(err);
+	          } else {
+	            let collection = db.collection(globals.INTERNSHIP_TABLE);
+	            collection.insert([internship], function (err, done) {
+	              if (err) {
+	                console.log('err@internships.js:acceptInternship() - ' + err);
+	                callback('SERVER_ERROR');
+	              } else {
+	                callback('INTERNSHIP_ACCEPTED');
+	              }
+	              db.close();
+	            });
+	          }
+	        }); // end createUserObject callback
+	      } // end MongoClient else branch
+	    }); // end MongoClient.connect
+	  } // end register else branch
+	}
 module.exports = router;
